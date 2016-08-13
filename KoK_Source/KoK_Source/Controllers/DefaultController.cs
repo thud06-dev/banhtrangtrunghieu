58671f8316e532a94e30cd1be1342a0168e47a38
RaitoYagamy <huynhlam1001@gmail.com>
2016-08-13 18:21:59 +0700

    up lai

---
+++ KoK_Source/KoK_Source/Controllers/DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KoK_Source.Models;
+using KoK_Source.Models.Banner;
+using KOKService;
+using System.Data;
+using System.IO;
+using KoK_Source.Com;
+using KoK_Source.Common;
+
+namespace KoK_Source.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ KoK_Source/banhtrangtrunghieu/Models/HomeModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using KOKService;
+
+namespace banhtrangtrunghieu.Models
+{
+    public class HomeModel
+    {
+        public string BANNER_DESC { get; set; }
+        public string BANNER_FILE { get; set; }
+    }
+}

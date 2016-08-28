7ba35d9e3ca1b2dd487ec887b5686f56a573995c
Nguyễn Viết Suối <Nguyn Vit Sui>
2016-08-28 18:07:15 +0700

    28/8 6h Suoi trasua

---
+++ KoK_Source/banhtrangtrunghieu/Com/HomeCom.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using KOKService;
+using banhtrangtrunghieu.Models;
+
+namespace banhtrangtrunghieu.Com
+{
+    public class HomeCom
+    {
+        private KOK_DATAEntities _kokDataEntities = new KOK_DATAEntities();
+        public List<NewsModel> getListPost()
+        {
+            List<NewsModel> model = new List<NewsModel>();
+            var dt = _kokDataEntities.KOK_NEWS.Take(5).OrderBy(m=>m.UPDATE_DATE);
+            if(dt != null)
+            {
+                foreach(var item in dt)
+                {
+                    NewsModel md = new NewsModel();
+                    md.NEWS_ID = item.NEWS_ID.ToString();
+                    md.NEWS_TITLE = item.NEWS_TITLE;
+                    md.NEWS_SEO_TITLE = item.NEWS_SEO_TITLE;
+                    md.NEWS_DESC = item.NEWS_DESC;
+                    md.NEWS_SEO_DESC = item.NEWS_SEO_DESC;
+                    md.NEWS_URL = item.NEWS_URL;
+                    md.NEWS_SEO_URL = item.NEWS_SEO_URL;
+                    md.NEWS_SEO_KEYWORD = item.NEWS_SEO_KEYWORD;
+                    md.NEWS_ORDER = item.NEWS_ORDER;
+                    md.NEWS_KEYWORD_ASCII = item.NEWS_KEYWORD_ASCII;
+                    md.CREATE_DATE = item.CREATE_DATE;
+                    md.UPDATE_DATE = item.UPDATE_DATE;
+                    md.CREATE_USER = item.CREATE_USER;
+                    md.UPDATE_USER = item.UPDATE_USER;
+                    md.ACTIVE = item.ACTIVE.GetValueOrDefault();
+                    model.Add(md);
+                }
+            }
+            return model;
+        }
+    }
+}
---
+++ KoK_Source/banhtrangtrunghieu/Models/HomeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using banhtrangtrunghieu.Models;
+
+namespace banhtrangtrunghieu.Models
+{
+    public class HomeModel
+    {
+        public List<NewsModel> ListNews;
+        public NewsModel News;
+        public string a { get; set; }
+    }
+}

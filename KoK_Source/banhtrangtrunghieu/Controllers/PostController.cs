5690a9ca3e4a666babd05b90b2c74f7a2beb687a
RaitoYagamy <huynhlam1001@gmail.com>
2016-09-04 09:39:39 +0700

    Lam: Mau link chi tiet tin tuc

---
+++ KoK_Source/banhtrangtrunghieu/Controllers/PostController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using banhtrangtrunghieu.Models;
+using banhtrangtrunghieu.Com;
+
+namespace banhtrangtrunghieu.Controllers
+{
+    public class PostController : Controller
+    {
+        PostCom _postCom = new PostCom();
+        // GET: Post
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DetailPost(string id_menu, string id_post)
+        {
+            try
+            {
+                NewsModel model = _postCom.detailNews(id_menu, id_post);
+                return View(model);
+            }
+            catch(Exception ex)
+            {
+                return new EmptyResult();
+            }
+        }
+    }
+}

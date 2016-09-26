65c814956bf9dbeff08a7dfb2800250470da7aa2
HUYHOANG\Hoang <nguyenngochuyhoangit@gmail.com>
2016-09-26 23:47:24 +0700

    commit new by hoang

---
+++ Source_New_Areas/KoK_Source/KoK_Source/Areas/Admin/Controllers/ConfigController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using KoK_Source.Areas.Admin.Models.Account;
+using KoK_Source.Areas.Admin.Models.Config;
+using KOKService;
+
+namespace KoK_Source.Areas.Admin.Controllers
+{
+    public class ConfigController : BaseController
+    {
+        private KOK_DATAEntities db = new KOK_DATAEntities();
+        private ConfigModel _config = new ConfigModel();
+
+        // GET: Admin/Config
+        public ActionResult Index()
+        {
+            List<ConfigModel> ls = _config.GetAllConfig();
+            return View(ls);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(ConfigModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                KOK_CONFIG config = new KOK_CONFIG
+                {
+                    CONFIG_TITLE = model.ConfigTitle,
+                    CONFIG_KEYWORD = model.ConfigKeyword,
+                    CONFIG_DESCRIPTION = model.ConfigDecription,
+                    CONFIG_HITCOUNTER = 0,
+                    //CONFIG_FAVICON = model.ConfigFavicon
+                };
+                try
+                {
+                    db.KOK_CONFIG.Add(config);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            return null;
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ConfigModel config = _config.GetConfigById(id);
+            return View(config);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(ConfigModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var config = db.KOK_CONFIG.FirstOrDefault(x => x.CONFIG_ID == model.Id);
+                if (config != null)
+                {
+                    config.CONFIG_TITLE = model.ConfigTitle;
+                    config.CONFIG_KEYWORD = model.ConfigKeyword;
+                    config.CONFIG_DESCRIPTION = model.ConfigDecription;
+                }
+
+                try
+                {
+                    db.Entry(config).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            return View();
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ConfigModel config = _config.GetConfigById(id);
+            return View(config);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            KOK_CONFIG acc = db.KOK_CONFIG.Find(id);
+            db.KOK_CONFIG.Remove(acc);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Source_New_Areas/KoK_Source/KoK_Source/Areas/Admin/Models/Config/ConfigModel.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using KoK_Source.Areas.Admin.Models.Account;
+using KoK_Source.Common;
+using KOKService;
+
+namespace KoK_Source.Areas.Admin.Models.Config
+{
+    public class ConfigModel
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [DisplayName("Tiêu đề")]
+        public string ConfigTitle { get; set; }
+
+        [DisplayName("Tiêu đề")]
+        public string ConfigKeyword { get; set; }
+
+        [DisplayName("Mô tả website")]
+        public string ConfigDecription { get; set; }
+
+        [DisplayName("Lượt truy cập")]
+        public int ConfigHitCounter { get; set; }
+
+        [DisplayName("Icon")]
+        public string ConfigFavicon { get; set; }
+
+        //public int ConfigOrder { get; set; }
+
+        //public string ConfigLanguage { get; set; }
+
+        //public string ConfigField1 { get; set; }
+
+        //public string ConfigField2 { get; set; }
+
+        //public string ConfigField3 { get; set; }
+
+        //public string ConfigField4 { get; set; }
+
+        //public string ConfigFielf5 { get; set; }
+
+        [DisplayName("State")]
+        public bool Active { get; set; }
+
+        [DisplayName("Create_Date")]
+        public string CreateDate { get; set; }
+
+        [DisplayName("Update Date")]
+        public string UpdateDate { get; set; }
+
+        [DisplayName("Create User")]
+        public string CreateUser { get; set; }
+
+        [DisplayName("Update User")]
+        public string UpdateUser { get; set; }
+
+
+        private KOK_DATAEntities db = new KOK_DATAEntities();
+        private CommonCnv _commonCnv = new CommonCnv();
+
+        public List<ConfigModel> GetAllConfig()
+        {
+            var data = db.KOK_CONFIG.ToList();
+            List<ConfigModel> config = new List<ConfigModel>();
+            foreach (var item in data)
+            {
+                config.Add(new ConfigModel
+                {
+                    Id = item.CONFIG_ID,
+                    ConfigTitle = item.CONFIG_TITLE ?? string.Empty,
+                    ConfigKeyword = item.CONFIG_KEYWORD ?? string.Empty,
+                    ConfigDecription = item.CONFIG_DESCRIPTION ?? string.Empty,
+                    ConfigHitCounter = item.CONFIG_HITCOUNTER ?? 0,
+                    ConfigFavicon = item.CONFIG_FAVICON ?? string.Empty,
+                    CreateDate = item.CREATE_DATE?.ToString() ?? string.Empty,
+                    CreateUser = item.CREATE_USER ?? string.Empty,
+                    UpdateDate = item.UPDATE_DATE?.ToString() ?? string.Empty,
+                    UpdateUser = item.UPDATE_USER ?? string.Empty,
+                    Active = _commonCnv.CnvBool(item.ACTIVE)
+                });
+            }
+            Console.WriteLine(config);
+            return config;
+        }
+
+        public ConfigModel GetConfigById(int? id)
+        {
+            var dt = db.KOK_CONFIG.FirstOrDefault(x => x.CONFIG_ID == id);
+            ConfigModel config = new ConfigModel();
+            if (dt != null)
+            {
+                config.Id = dt.CONFIG_ID;
+                config.ConfigTitle = dt.CONFIG_TITLE;
+                config.ConfigKeyword = dt.CONFIG_KEYWORD;
+                config.ConfigDecription = dt.CONFIG_DESCRIPTION;
+                config.Active = _commonCnv.CnvBool(dt.ACTIVE);
+            }
+            return config;
+        }
+
+       
+    }
+}
